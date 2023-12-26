c0ca4eefec0c811c3915f0fa5595e89b6e37a4b5
Muhammad Noman <m.nouman.nomi@gmail.com>
2023-12-26 23:48:07 +0500

    Admin authintication feature added: register,login

---
+++ src/Routers/AuthRouter/AuthRouter.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import AuthController from "../../Controller/AuthController/AuthController.js";
+import AuthValidator from "../../Validator/AuthValidator/AuthValidator.js";
+
+const AuthRouter = Router();
+
+AuthRouter.post(
+  "/register/",
+  AuthValidator.registerAdmin,
+
+  AuthController.registerAdmin
+);
+AuthRouter.post("/login/", AuthValidator.loginAdmin, AuthController.loginAdmin);
+
+export default AuthRouter;
